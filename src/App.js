32f352a36716401ed862052bce9c2eedd5356c0e
vmadaraj <vmadaraj@asu.edu>
2019-02-18 23:25:33 -0700

    Todo app initial structure with Delete and Toggle options

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/layouts/Header';
+import Todos from './components/Todos';
+
 import './App.css';
 
+
 class App extends Component {
+  state = {
+    todos : [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with GF',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+  //toggle complete
+  markComplete = (id) => {
+    this.setState ({ todos: this.state.todos.map (todo => {
+      if(todo.id === id ){
+          todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  //Delete Todo
+  delTodo = (id) => {
+    this.setState ({ todos: [...this.state.todos.filter( todo => todo.id!==id)]});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Todos todos={this.state.todos} markComplete = {this.markComplete}
+        delTodo = {this.delTodo} />
       </div>
     );
   }
